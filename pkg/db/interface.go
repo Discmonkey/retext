52ec8ebbc3e18814337e1c58f75259c122a6676b
ADMadera <angel.d.amdera@gmail.com>
2020-05-20 15:45:25 -0400

    [retext-1] create basic association component
    extend db interface(?)

--- pkg/db/interface.go
+++ pkg/db/interface.go
@@ -1,9 +1,17 @@
 package db
 
 type ID = string
+type Category struct {
+	id   ID
+	name string
+}
 
 type Store interface {
 	UploadFile(filename string, contents []byte) (ID, error)
 	GetFile(id ID) ([]byte, error)
 	Files() ([]ID, error)
+	CreateCategory(name string) (Category, error)
+	CategorizeText(categoryId ID, documentTexts []map[ID]string) error
+	GetCategory(ID) (Category, error)
+	Categories() ([]ID, error)
 }
