389ca82c7b47c3af837a66ec574ee61fd6d9ff86
ADMadera <angel.d.amdera@gmail.com>
2020-05-20 22:43:27 -0400

    [retext-1] create basic association component
    implement new store methods

--- pkg/db/file_impl_test.go
+++ pkg/db/file_impl_test.go
@@ -7,9 +7,9 @@ import (
 
 // TestFSBackend covers all the interface methods
 func TestFSBackend(t *testing.T) {
-	testDirName := "/tmp/filetest"
+	testDirName := "c:/windows/temp/filetest"
 
-	_ = os.RemoveAll("/tmp/filetest")
+	_ = os.RemoveAll(testDirName)
 
 	store := &FSBackend{}
 
@@ -36,7 +36,8 @@ func TestFSBackend(t *testing.T) {
 	}
 
 	contents := []byte("hello")
-	key, err := store.UploadFile("test1.txt", contents)
+	testFileName := "test1.txt"
+	key, err := store.UploadFile(testFileName, contents)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -65,6 +66,41 @@ func TestFSBackend(t *testing.T) {
 		}
 	}
 
+	//todo: if your "categoriesFile" has categories added by using the website, the `len(cats) != 1` test will fail.
+	testCategoryName := "test"
+	c, err := store.CreateCategory(testCategoryName)
+	if err != nil {
+		t.Fatalf("failed to save category: %s", err)
+	}
+
+	c2, err := store.GetCategory(c.ID)
+	if err != nil {
+		t.Fatalf("failed to get category: %s", err)
+	}
+	if c2.Name != testCategoryName {
+		t.Fatalf("category came back with unexpected name: %s", err)
+	}
+	//todo: write a test that looks for a non-existent category
+
+	testText := "made up text"
+	err = store.CategorizeText(c.ID, testFileName, testText)
+	if err != nil {
+		t.Fatalf("failed to categorize text: %s", err)
+	}
+	c2, _ = store.GetCategory(c.ID)
+	if len(c2.Texts) == 0 {
+		t.Fatal("failed to categorize text")
+	}
+
+	cats, err := store.Categories()
+	if err != nil {
+		t.Fatalf("failed to get list of categories: %s", err)
+	}
+	//this check for 1 will break if you
+	if len(cats) != 1 {
+		numCats := string(len(cats))
+		t.Fatalf("incorrect number of categories; got: %s", numCats)
+	}
 	_ = os.Remove("/tmp/filetest")
 
 }
