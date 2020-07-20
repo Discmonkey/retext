99aab4fad13586381b9901e4a6173eb2bfc5fa59
max <mgrinchenko@progeny.net>
2020-07-19 21:17:53 -0400

    color bullet class for marking up paragraphs

---
+++ pkg/www/retext/src/components/display/ColorBullet.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :style="style">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ColorBullet",
+    props: ["color"],
+    computed: {
+        style() {
+            return `border-radius: 50%; background-color: ${this.color}; width: 1.5em; height: 1.5em; margin-bottom: 2px`;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
