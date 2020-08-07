7405818907fdadf75f34b56dbda914d080c029d0
ADMadera <angel.d.amdera@gmail.com>
2020-08-07 00:28:22 -0400

    [20] implement basic breadcrumbs

--- pkg/www/retext/src/components/nav/Breadcrumbs.vue
+++ pkg/www/retext/src/components/nav/Breadcrumbs.vue
@@ -1,19 +1,28 @@
 <template>
-    <ul class="breadcrumb" v-if="$route.matched.length" >
-        <li v-for="(crumb, i) in $route.matched" :key="i" class="breadcrumb-item">
-            <router-link v-if="i < $route.matched.length-1" :to="crumb">{{ crumb.name }}</router-link>
+    <ul class="breadcrumb" v-if="crumbs.length" >
+        <li v-for="(crumb, i) in crumbs" :key="i" class="breadcrumb-item" :class="{'active': isLast(i)}">
+            <router-link v-if="!isLast(i)" :to="crumb">{{ crumb.name }}</router-link>
             <span v-else class="last">{{ crumb.name }}</span>
         </li>
     </ul>
 </template>
 
 <script>
-/*=============================================================================
-  started with this... doesn't much resemble it anymore. consider it a url-parsing-alternative...
-  https://github.com/NxtChg/pieces/blob/master/js/vue/vs-crumbs/vs-crumbs.js
-=============================================================================*/
-
-export default {
-    name: "Breadcrumbs",
-}
+    /*=============================================================================
+      started with this link... doesn't much resemble it anymore. consider it a url-parsing-alternative...
+      https://github.com/NxtChg/pieces/blob/master/js/vue/vs-crumbs/vs-crumbs.js
+    =============================================================================*/
+    export default {
+        name: "Breadcrumbs",
+        computed: {
+            crumbs() {
+                return this.$route.matched;
+            },
+        },
+        methods: {
+            isLast(i) {
+                return i === this.crumbs.length - 1;
+            }
+        }
+    }
 </script>
