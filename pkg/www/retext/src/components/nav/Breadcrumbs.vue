2f4ce5b7e08cc51ff209517a431e14af9b02291b
ADMadera <angel.d.amdera@gmail.com>
2020-08-07 00:11:55 -0400

    [20] implement basic breadcrumbs
    Uses default bootstrap styling.
    
    changed how routes are organized to make dynamic breadcrumbs easy. Did have some trouble at first but all the issues seem solved now... I'd have liked to solve this without the "empty" Home component but the transitions between pages still seem smooth to me so, not too bad?
    
    Be aware of the url/page changing(or not) when trying to go between "/" and "/upload" and vice versa. This is not unexpected (/upload is the desired home page, after all)

---
+++ pkg/www/retext/src/components/nav/Breadcrumbs.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="breadcrumb" v-if="$route.matched.length" >
+        <li v-for="(crumb, i) in $route.matched" :key="i" class="breadcrumb-item">
+            <router-link v-if="i < $route.matched.length-1" :to="crumb">{{ crumb.name }}</router-link>
+            <span v-else class="last">{{ crumb.name }}</span>
+        </li>
+    </ul>
+</template>
+
+<script>
+/*=============================================================================
+  started with this... doesn't much resemble it anymore. consider it a url-parsing-alternative...
+  https://github.com/NxtChg/pieces/blob/master/js/vue/vs-crumbs/vs-crumbs.js
+=============================================================================*/
+
+export default {
+    name: "Breadcrumbs",
+}
+</script>
