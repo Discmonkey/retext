f838aafd97cc167ea97257689700094c04c07ffe
max <frismo98@gmail.com>
2020-12-25 00:30:29 -0500

    migrations added to deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,14 +3,15 @@ ip=$2
 ssh="ssh -i $pem_location ec2-user@${ip}"
 release_dir=deployment/releases/$(date "+%F-%T")
 
-rm -rf $release_dir ${release_dir}.tar
+rm -rf $release_dir
 make docker_server
+
 mkdir $release_dir
 
 docker save -o ${release_dir}/qode.tar qode
 
 scp -r -i $pem_location $release_dir/qode.tar ec2-user@${ip}:/home/ec2-user/release/
-
+scr -r -i $pem_location deployment/qode/migrations/* ec2-user@${ip}:/home/ec2-user/release/migrations/
 ${ssh} "docker load -i ./release/qode.tar"
 ${ssh} "pushd release/deployment && docker-compose down && docker-compose up -d"
 
